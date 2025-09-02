7fb44edaf217b9f6d153898d3303f532faecdf2f
Oleh Koval <oleg10koval@gmail.com>
2025-09-02 18:29:46 +0300

    feat: map course pricing and duration

--- wplms-s1-importer/includes/IdMap.php
+++ wplms-s1-importer/includes/IdMap.php
@@ -6,20 +6,31 @@ class IdMap {
 
     public function __construct() {
         $this->map = \get_option( \WPLMS_S1I_OPT_IDMAP, [
-            'courses'     => [],
-            'units'       => [], // mapped to Lessons in LearnDash
-            'quizzes'     => [],
-            'assignments' => [],
-            'certificates'=> [],
+            'courses'      => [],
+            'units'        => [], // mapped to Lessons in LearnDash
+            'quizzes'      => [],
+            'assignments'  => [],
+            'certificates' => [],
         ] );
     }
 
     public function get_all() { return $this->map; }
 
-    public function get( $type, $old_id ) { return isset( $this->map[ $type ][ $old_id ] ) ? (int) $this->map[ $type ][ $old_id ] : 0; }
+    public function get_entry( $type, $old_id ) {
+        return isset( $this->map[ $type ][ $old_id ] ) ? $this->map[ $type ][ $old_id ] : null;
+    }
 
-    public function set( $type, $old_id, $new_id ) {
-        $this->map[ $type ][ (string) $old_id ] = (int) $new_id;
+    public function get( $type, $old_id ) {
+        $entry = $this->get_entry( $type, $old_id );
+        return $entry ? (int) array_get( $entry, 'id', 0 ) : 0;
+    }
+
+    public function set( $type, $old_id, $new_id, $slug = '' ) {
+        $this->map[ $type ][ (string) $old_id ] = [
+            'id'          => (int) $new_id,
+            'slug'        => (string) $slug,
+            'imported_at' => time(),
+        ];
         \update_option( \WPLMS_S1I_OPT_IDMAP, $this->map, false );
     }
 
